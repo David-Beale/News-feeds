f08abc227ad698fd6bc9043ad33734d922c7bbd1
David B <59053870+David-Beale@users.noreply.github.com>
2021-07-18 16:20:45 +0100

    delete refresh button

--- src/components/App/Header/Header.js
+++ src/components/App/Header/Header.js
@@ -2,14 +2,12 @@ import React from "react";
 
 import SignOutButton from "./Components/SignOutButton/SignOutButton";
 import AddNewFeedButton from "./Components/AddNewFeedButton/AddNewFeedButton";
-import RefreshFeedButton from "./Components/RefreshFeedButton/RefreshFeedButton";
 import { HeaderContainer } from "./HeaderStyle";
 
 export default function Header() {
   return (
     <HeaderContainer>
       <AddNewFeedButton />
-      {/* <RefreshFeedButton /> */}
       <SignOutButton />
     </HeaderContainer>
   );
