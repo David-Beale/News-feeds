75290001397b107af32bd34e6fcb8330f5e00a75
David B <59053870+David-Beale@users.noreply.github.com>
2021-07-24 14:20:08 +0100

    Updates

---
+++ precache-manifest.a6481eb9d9807f241403d8cd0a506166.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f49ff3b4b837b101c27f85558a29bd",
+    "url": "/News-feeds/index.html"
+  },
+  {
+    "revision": "2b28139fe3211b3be325",
+    "url": "/News-feeds/static/css/main.599fd114.chunk.css"
+  },
+  {
+    "revision": "ec36f760f1cd806d59e2",
+    "url": "/News-feeds/static/js/2.8300ba17.chunk.js"
+  },
+  {
+    "revision": "3c36f3e9f44745d53f6cad195a4e5d6a",
+    "url": "/News-feeds/static/js/2.8300ba17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b28139fe3211b3be325",
+    "url": "/News-feeds/static/js/main.9927acf5.chunk.js"
+  },
+  {
+    "revision": "c9596417652b4e7885f1",
+    "url": "/News-feeds/static/js/runtime-main.681d492c.js"
+  }
+]);
